b4f74e457feaec6b29b391a26a683ad862da4051
Jaldiver <145017897+Jaldiver@users.noreply.github.com>
2024-04-09 20:01:36 -0500

    Add files via upload

---
+++ Masa/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Console.WriteLine("Digite su peso porfavor");
+
+int Peso = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ahora su altura");
+
+float Alt = float.Parse(Console.ReadLine());
+
+float MC = (Peso/(Alt*Alt));
+
+Console.WriteLine("Su MC es de: " + MC);
+*/
+
+/*
+Console.WriteLine("Digite un dia del año (0-365)");
+int Dia = int.Parse(Console.ReadLine());
+
+int Semana = (Dia/7)+1;
+int Mes = (Dia/30);
+
+Console.WriteLine("Se encuentra en la semana: " + Semana);
+Console.WriteLine("Se encuentra en el mes: " + Mes);
+*/
+
+/*
+int Base = 10;
+int Altura =15;
+
+Perimetro = (Base+Altura)*2;
+*/
+
+
+Console.WriteLine("Digite los grados C° que desea se conviertan en F°");
+
+int Celc = int.Parse(Console.ReadLine());
+
+float Far = (Celc*(9/5)) + 32;
+
+Console.WriteLine("La temperatura en grados F° es: " + Far);
+
+
+/*
+Console.WriteLine("Digite 3 valores porfavor");
+int Va1 = int.Parse(Console.ReadLine());
+int Va2 = int.Parse(Console.ReadLine());
+int Va3 = int.Parse(Console.ReadLine());
+
+float Prom = ((Va1+Va2+Va3)/3);
+int Sum = (Va1+Va2+Va3);
+
+Console.WriteLine("El promedio es de: " + Prom);
+Console.WriteLine("La sumatoria es de: " + Sum);
+Console.WriteLine("La multiplicacion entre estos 2 es de: " + (Prom*Sum));
+*/
