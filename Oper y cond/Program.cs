df7a02cc56dbaf79424c2b18cbb3050254f8066c
Jaldiver <145017897+Jaldiver@users.noreply.github.com>
2024-04-09 20:02:18 -0500

    Add files via upload

---
+++ Oper y cond/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+
+bool esDelTdea = false;
+if(esDelTdea==true){
+    Console.WriteLine("Bienvenido estudiante del TdeA");
+}else{
+    Console.WriteLine("No es estudiante del TdeA");
+}
